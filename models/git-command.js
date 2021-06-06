20edb6ac9b180fd8308f11f0ed888185c7d2a503
jbugzy <jcsbugnay@gmail.com>
2021-06-06 15:36:52 +0800

    These are the originals

---
+++ models/git-command.js
@@ -0,0 +1,63 @@
+class GitCommand {
+    constructor(working_directory){
+        this.working_directory = working_directory;
+    }
+    //Command: git init 
+    init(){
+        this.staging = [];
+        this.local_repository = [];
+        return "Initialized as empty Git repository.";
+    }
+
+    //Command: git status
+    status(){
+        let result = "", count = 0;
+        for(let file in this.working_directory.new_changes){
+            count++;
+            result += `\n${file}`;
+        }
+        if(count == 0){
+            result = "\n";
+        }
+        return `You have ${count} change/s.`+result;
+    }
+
+    //Command: git add <filename/file directory/wildcard> 
+    add(path_file){
+        let modified_files = this.working_directory.new_changes;
+        
+        if(modified_files[path_file]){
+            this.staging.push(modified_files[path_file]);
+            delete modified_files[path_file];
+        }else if(path_file == "."){
+            delete this.working_directory.new_changes;
+            delete modified_files[path_file];
+        }else{
+            return `Failed to add ${path_file}! File is not modified or missing.`;
+        }
+        return "Successfully added as index file/s.";
+    }
+
+    //Command: git commit -m "<message>"
+    commit(message){
+        if(this.staging.length > 0){
+            this.local_repository.push({ "message": message, "files": this.staging });
+            this.staging = [];
+            return "Done committing to local repository.";
+        }
+        return "Nothing to commit.";
+    }
+
+    //Command: git push
+    push(){   
+        if(this.local_repository.length > 0){
+            return "Done pushing to remote repository.";
+        } 
+        else {
+            return "Nothing to push. No committed file found.";
+        }     
+    }
+}
+
+
+module.exports = GitCommand;
