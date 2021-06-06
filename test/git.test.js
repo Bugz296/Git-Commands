36467c10172b05e7ec637025534b294d4fb67cef
jbugzy <jcsbugnay@gmail.com>
2021-06-06 15:45:48 +0800

    With Features

--- test/git.test.js
+++ test/git.test.js
@@ -48,4 +48,18 @@ describe("Testing add()", function(){
         expect(output_add).to.equal('Successfully added as index file/s.');
         expect(output_status).to.equal('You have 0 change/s.\n');
     });
+
+    it('Should success with path file "*"', function(){
+        let wd = new WorkingDirectory();
+        wd.addFile("actions.yml", ".github/workflows", "");
+
+        let git = new GitCommand(wd);
+        git.init();
+
+        let output_add = git.add("*");
+        let output_status = git.status();
+
+        expect(output_add).to.equal('Successfully added as index file/s.');
+        expect(output_status).to.equal('You have 1 change/s.\n.github/workflows/actions.yml');
+    });
 })
